3532c93212f2eed4673dae67e1e986a0c4519e43
Andrzej <andrzej@imprv.ai>
2025-11-05 15:37:22 +0100

    add code to simulate mypy issue

--- masterclass/main.py
+++ masterclass/main.py
@@ -1,6 +1,11 @@
 def main():
     print("Hello, World!")
 
-
 if __name__ == "__main__":
+    print("Hello, World! This is my first GitHub Action! 🚀")
+    user_names_index : dict[int, str] = {}
+    user_names_index[1] = "John"
+    user_names_index[2] = "Jane"
+    # user_names_index[3] = 23 # dodaję liczbę do słownika z tekstem 
+
     main()
